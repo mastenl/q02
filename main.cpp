8bd464ba34b5ad1fd5c4aeb7d8e297289cc1d8d4
mastenl <mleonard14@comcast.net>
2017-10-04 17:17:26 -0700

    Backing up answer to github again

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
   vector <vector <string> > wordGrid;
-
+  vector <string> word;
   /*
      do not edit this space
 
@@ -15,7 +15,7 @@ int main()
      add the code that gets the user's input and
      inserts the input into every element of the
      matrix wordGrid
-  */
+ */
 
   //PUT YOUR CODE BELOW THIS LINE
   //that prints each element of the matrix in the
@@ -24,18 +24,16 @@ int main()
   //the first and second blanks should be replaced with the coordinates
   //of the element's row and column and the third blank should be replaced
   //with the value of that element
-wordGrid[1][1] = "meme4";
-
-    if(wordGrid.size()>0 && wordGrid[0].size()>0)
-        {
+    
+word.push_back("1234");
+wordGrid.push_back(word);
          for(int i =0; i < wordGrid.size();i++)
          {
             for(int j =0;j<wordGrid[i].size();j++)
            {
-           cout<<"Row "<<i<<"Column "<<j<<"is "<<wordGrid[i][j]<<endl;
+           cout<<"Row "<<i<<" Column "<<j<<" is  "<<wordGrid[i][j]<<endl;
            }
-         }
-        }
+          } 
 
 
 
